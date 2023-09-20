1dcaa42c99a37badb9d005b74b0b58b55e67693c
Pavankumar Guttedar <pavankumar494748@gmail.com>
2023-09-20 21:49:21 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter, Router, Route } from "react-router-dom";
+
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
+import Category from "./components/Category/Category";
+import SingleProduct from "./components/SingleProduct/SingleProduct";
+import Newsletter from "./components/Footer/Newsletter/Newsletter";
+import AppContext from "./utils/context";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/category/:id" element={<Category />} />
+        <Route path="/product/:id" element={<SingleProduct />} />
+      </Route>
+    </BrowserRouter>
+  );
+}
+
+export default App;
